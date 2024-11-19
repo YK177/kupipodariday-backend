dde661af3b818dce53c5f4be662745b7c3937349
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-11-19 23:48:40 +0300

    Add entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { Offer } from './offers/entities/offer.entity';
 
 @Module({
   imports: [
@@ -16,8 +20,9 @@ import { OffersModule } from './offers/offers.module';
       username: 'student',
       password: 'student',
       database: 'kupipodariday',
-      entities: [],
+      entities: [User, Wish, Wishlist, Offer],
       synchronize: true,
+      logging: true,
     }),
     UsersModule,
     WishesModule,
--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,41 @@
-export class Offer {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Wish } from '../../wishes/entities/wish.entity';
+
+@Entity()
+export class Offer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.offers)
+  user: User;
+
+  @ManyToOne(() => Wish, (wish) => wish.offers)
+  item: Wish;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  amount: number;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+  })
+  hidden: boolean;
+}
