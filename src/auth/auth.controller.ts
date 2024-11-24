fc0d68a7e4e998ca35f02f89fe947225a7e4d7a4
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-11-24 20:59:49 +0300

    add auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create() {
+    return this.authService.create();
+  }
+
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string) {
+    return this.authService.update(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(
-    cors({
-      origin: ['http://localhost:3001'],
-      methods: ['*'],
-      allowedHeaders: ['*'],
-    }),
-  );
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
