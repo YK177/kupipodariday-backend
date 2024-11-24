7ecce9a1d08c6fe9ac75aff0dec42e33b53056d6
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-11-24 20:47:30 +0300

    Set cors politics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: ['http://localhost:3001'],
+      methods: ['*'],
+      allowedHeaders: ['*'],
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
