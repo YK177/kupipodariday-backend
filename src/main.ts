9ff59299b70a8f126707410e3e7b140f5da73c85
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-12-23 15:43:50 +0300

    chore: update backend environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
